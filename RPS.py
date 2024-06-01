188833bc793aa1c5cd8024a10be5f5b6f435b7cd
Sayan Bhadra <sayanbhadra20@gmail.com>
2024-06-02 01:35:32 +0530

    Delete RPS.py

--- RPS.py
+++
@@ -1,8 +0,0 @@
-import random
-option = ('rock', 'paper', 'scissors')
-computer = random.choice(option)
-player = input("Rock paper or scissors?")
-print("Computer: ",computer)
-print("Player: " ,player)
-if computer == player:
-    print("Its a tie!")
