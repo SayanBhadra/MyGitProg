024045613ef197f2ca7be691cfbff2860eb273ed
SayanBhadra <sayanbhadra20@gmail.com>
2024-06-15 12:18:50 +0530

    Added a new project

---
+++ RPS.py
@@ -0,0 +1,48 @@
+import random
+while True:
+    option = ('rock', 'paper', 'scissors')
+    computer = random.choice(option)
+    player = None
+    while player not in option:
+        player = input("Rock paper or scissors?").lower()
+
+    if computer == player:
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Its a tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("you lose")
+        if computer == "scissors":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("you win")
+    elif player == "paper":
+        if computer == "scissors":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("you lose")
+        if computer == "rock":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("you win")
+    elif player == "scissors":
+        if computer == "paper":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("you lose")
+        if computer == "rock":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("you win")
+
+
+    choice = input("Do you want to play again? ")
+    if choice != "yes":
+        break
+print("bye")
+
+
+
